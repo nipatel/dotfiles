3e98221c40300a0c39efae7ee22969b0027cf7a3
Christopher Clark <cfc2a@sneezy.(none)>
2013-03-01 15:26:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Chris's dotfiles
+
+Dotfiles for all my Ubuntu boxes.
+
+# Thanks
+
+[Mathias Bynens](http://mathiasbynens.be) for his popular [dotfiles](http://mths.be/dotfiles) project
---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# cd to the dirictory this script is in
+cd "$(dirname "${BASH_SOURCE}"")
+
+# get the latest and greatest from my baller git repo
+git pull
+
+# copy everything in this folder to your $HOME folder
+function copyToHome() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+}
+
+# just a little check to make sure you really want to do the rsync
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	copyToHome
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		copyToHome
+	fi
+fi
+
+# not sure why this needs to be here
+unset copyToHome
+
+# because .profile wouldn't be executed again until next time you log in.
+# No need to source .bashrc, because I do it in .profile
+source ~/.profile
